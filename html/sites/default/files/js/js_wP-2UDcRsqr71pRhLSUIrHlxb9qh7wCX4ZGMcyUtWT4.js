e848112aa32a8313e137356175819d29d017fa91
Matthew Obert <github@hotwebmatter.com>
2020-02-21 17:48:24 -0500

    Commit Tome export

---
+++ html/sites/default/files/js/js_wP-2UDcRsqr71pRhLSUIrHlxb9qh7wCX4ZGMcyUtWT4.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.procomrad_barrio = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
+;
